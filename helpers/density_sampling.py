3d53092586029a397354ac8aa7283663af12e3f2
Sanjog <st0128@uah.edu>
2023-01-06 12:45:38 -0600

    density sampling script added

---
+++ helpers/density_sampling.py
@@ -0,0 +1,23 @@
+import numpy as np
+# 100% of data can be used to show
+# 50% of data can be used to show
+# 25% of data can be used to show
+
+# many algorithms can be used to sample data based off density
+# for now I will just take the data in an equal intervals.
+
+def sample_data(data):
+    """
+    Samples the data set, by picking the data in equal intervals. 
+    Currently takes 1 out of 5 data sets
+    TODO: Explore other algorithms to sample the data.
+
+    Args:
+        data (array): The data set that is to be sampled
+
+    Returns:
+        data (array): The sampled data set 
+    """
+    np_data = np.array(data)    
+    return np_data[::5]
+    

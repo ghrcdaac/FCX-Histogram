1a7a97760aa901b596028861910255d2aa604b08
Sanjog Thapa <sanzog03@gmail.com>
2023-01-13 09:32:00 -0600

    added script for lambda deployment using docker and ecr.

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Vars
+account_id="307493436926"
+region="us-east-1"
+repository_name="histogram-preprocessing-v1"
+#create new ECR repo
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${region}.amazonaws.com
+aws ecr create-repository --repository-name ${repository_name} --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
+# docker build image, tag it and push it to the ECR repo
+docker build -t ${repository_name} .
+docker tag  ${repository_name}:latest ${account_id}.dkr.ecr.${region}.amazonaws.com/${region}:latest
+docker push ${account_id}.dkr.ecr.${region}.amazonaws.com/${repository_name}:latest

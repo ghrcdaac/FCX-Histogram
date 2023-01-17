89214ece9694427780ef4bd1765e4b1808f47aea
Sanjog <st0128@uah.edu>
2023-01-17 12:07:29 -0600

    density based sampling fix. Used negative linear relation as a density function.

--- helpers/density_sampling.py
+++ helpers/density_sampling.py
@@ -1,11 +1,5 @@
 import numpy as np
 import math
-# 100% of data can be used to show
-# 50% of data can be used to show
-# 25% of data can be used to show
-
-# many algorithms can be used to sample data based off density
-# for now I will just take the data in an equal intervals.
 
 class DensitySampling:
     def __init__(self, density=1.0):
@@ -20,8 +14,10 @@ class DensitySampling:
     def sample_data(self, data):
         """
         Samples the data set, by picking the data in equal intervals. 
-        Currently takes 1 out of 5 data sets
-        TODO: Explore other algorithms to sample the data.
+        Note: density and skip_rate are inversly proportional to each other
+        So, applying -ve linear relation between them, we come with an equation
+        Say, L = length of array, d = density wanted, g = skip_interval(gap)
+        We have, g = (1-L)*d + L
 
         Args:
             data (array): The data set that is to be sampled
@@ -30,8 +26,8 @@ class DensitySampling:
             data (array): The sampled data set 
         """
         np_data = np.array(data)
-        
-        #  using the density value and total length of input data, find the skip rate.
-        skip_rate = math.floor(np_data.size * self.density)
+        total_size = np_data.size
+
+        skip_rate = math.floor(((1 - total_size) * self.density) + total_size)
         return np_data[::skip_rate].tolist()
     
